a9985ce8a6f16a2eca14973bde480a7c2ac803e7
IT022304 <it022304@IT022304s-MacBook-Air.local>
2023-03-20 14:51:53 -0700

    Calculator operations modified

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,13 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
+
 function backspace(){
     let number = document.getElementById("result").value;
-    document.getElementById("result").value = number.substring(0,number.length - 1);
+    if(number[number.length-1] == '+'||number[number.length-1] == '-'||number[number.length-1] == '*'||number[number.length-1] == '/'){
+        co--;
+    }
+    document.getElementById("result").value = number.substring(0,number.length -1);
 }
 
 var operatorArray =[];
@@ -11,22 +15,35 @@ var numberArray =[];
 var res = [];
 var co=0
 let cd=0
-var str=""
+
 function display(value){
     var inputValue=document.getElementById("result").value;
-    if(inputValue[0]=='.'){
-        document.getElementById("result").value = '0'+document.inputValue ;
+    if(value=='.'){
+        if(inputValue[0]=='.'){
+            document.getElementById("result").value = '0'+inputValue ;
+        }
+        else if(inputValue[inputValue.length-1]=='+'||inputValue[inputValue.length-1]=='-'||inputValue[inputValue.length-1]=='*'||inputValue[inputValue.length-1]=='/')
+        {
+            
+            document.getElementById("result").value = inputValue+"0" ;  
+        }
     }
     else if(inputValue[0]=='+'||inputValue[0]=='-'||inputValue[0]=='*'||inputValue[0]=='/'||inputValue[0]=='='){
-        document.getElementById("result").value = 0
+        document.getElementById("result").value = ""
         alert("Please enter a number first!")
     }
     if ((value == "+")||(value == "-")||(value == "*")||(value == "/")||(value == "=")){
+        if(inputValue[inputValue.length-1]=='.'){
+            document.getElementById("result").value = inputValue+"0" ;  
+        }
+
         if(co==0){
-            document.getElementById("result").value += value;
-        } 
-    co++;
-    cd=0;
+            console.log(inputValue[inputValue.length-1]);
+            document.getElementById("result").value += value; 
+            } 
+            
+        co++;
+        cd=0;
     }
 
     else if (value==".") {
@@ -41,15 +58,11 @@ function display(value){
     }
 }
 
-
 function calculate() {
     var input = document.getElementById("result").value;
-
     var number_array = (input.split(/[-*+/]+/))
     var operator_array = input.split(/[1234567890]+/);
     operator_array = operator_array.slice(1,operator_array.length-1)
-    console.log(number_array)
-    console.log(operator_array)
     operations=[]
     for (let i=0; i<operator_array.length; i++){
         if (operator_array[i]!=='.'){
@@ -58,6 +71,9 @@ function calculate() {
     }
     console.log(operations);
     let res=Number(number_array[0])
+    if(operations.length==0){
+        res=Number(number_array[0])
+    }
     for (let i=0; i<operations.length; i++){
         if (operations[i]==='+'){
             res+=Number(number_array[i+1])
